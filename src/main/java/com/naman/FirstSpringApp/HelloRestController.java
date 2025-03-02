2da7cc861a94c2d40a7cf5e8e23cc27cee1fc2f2
Naman Agarwal <101493202+NamanAgarwal08@users.noreply.github.com>
2025-03-02 15:12:20 +0530

    Add files via upload

---
+++ src/main/java/com/naman/FirstSpringApp/HelloRestController.java
@@ -0,0 +1,25 @@
+package com.arnamanchi.FirstSpringApp;
+
+import org.springframework.web.bind.annotation.*;
+import com.naman.FirstSpringApp.User;
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+    @RequestMapping(value = {"", "/", "/home"})
+    public String sayHello() {
+        return "Hello from BridgeLabz!";
+    }
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHelloQuery(@RequestParam("name") String name) {
+        return "Hello " + name + "!";
+    }
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
+    }
+
+}
